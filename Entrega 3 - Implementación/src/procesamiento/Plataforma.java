cc9c508adc10e4ed53a72ebc76850a76a89d9b03
Estudiante <Estudiante@172.24.101.105>
2022-03-31 10:28:33 -0500

    Creacion de clases

---
+++ Entrega 3 - Implementación/src/procesamiento/Plataforma.java
@@ -0,0 +1,12 @@
+package procesamiento;
+
+import java.util.ArrayList;
+
+public class Plataforma
+{
+	static ArrayList<String[]> matrizParticipantes = new ArrayList<String[]>();
+	public participante registrarParticipante()
+	{
+	
+	}
+}
